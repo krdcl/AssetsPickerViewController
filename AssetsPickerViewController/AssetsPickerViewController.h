e21314fe8a7e8b804b4fc6107cbb7500144d0cdb
Maksims Grejs <maksims@cubemobile.lv>
2021-05-13 19:42:57 +0300

    try add Carthage support

---
+++ AssetsPickerViewController/AssetsPickerViewController.h
@@ -0,0 +1,18 @@
+//
+//  AssetsPickerViewController.h
+//  AssetsPickerViewController
+//
+//  Created by MacWorker on 13.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AssetsPickerViewController.
+FOUNDATION_EXPORT double AssetsPickerViewControllerVersionNumber;
+
+//! Project version string for AssetsPickerViewController.
+FOUNDATION_EXPORT const unsigned char AssetsPickerViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AssetsPickerViewController/PublicHeader.h>
+
+
